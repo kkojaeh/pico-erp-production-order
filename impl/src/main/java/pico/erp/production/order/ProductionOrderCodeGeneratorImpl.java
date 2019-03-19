e7f9f85ca5623c8f97b105d7cc1f06ed3fc4b7c3
Ethan <kkojaeh@gmail.com>
2019-03-19 10:50:02 +0900

    코드 생성 방식 변경 및 인덱스 생성

--- impl/src/main/java/pico/erp/production/order/ProductionOrderCodeGeneratorImpl.java
+++ impl/src/main/java/pico/erp/production/order/ProductionOrderCodeGeneratorImpl.java
@@ -2,6 +2,7 @@ package pico.erp.production.order;
 
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.time.temporal.TemporalAdjusters;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -17,13 +18,12 @@ public class ProductionOrderCodeGeneratorImpl implements ProductionOrderCodeGene
   @Override
   public ProductionOrderCode generate(ProductionOrder productionOrder) {
     val now = OffsetDateTime.now();
-    val begin = now.with(LocalTime.MIN);
-    val end = now.with(LocalTime.MAX);
+    val begin = now.with(TemporalAdjusters.firstDayOfMonth()).with(LocalTime.MIN);
+    val end = now.with(TemporalAdjusters.lastDayOfMonth()).with(LocalTime.MAX);
     val count = productionOrderRepository.countCreatedBetween(begin, end);
-    val date =
-      Integer.toString(now.getYear() - 1900, 36) + Integer.toString(now.getMonthValue(), 16)
-        + Integer.toString(now.getDayOfMonth(), 36);
-    val code = String.format("%s-%04d", date, count + 1).toUpperCase();
+    val code = String
+      .format("PRO%03d%02d-%04d", now.getYear() % 1000, now.getMonthValue(), count + 1)
+      .toUpperCase();
     return ProductionOrderCode.from(code);
   }
 }
---
+++ impl/src/main/resources/production-order/flyway/mysql/V3__created-date-index.sql
@@ -0,0 +1,2 @@
+create index IDXlqfb2ppe6bumr2j9ie3cpesjd
+	on pdo_production_order (created_date);
